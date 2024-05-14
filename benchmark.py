3135f44ca597a463f88b0b21bb57fd721f117ccf
Tailing Yuan <yuantailing@gmail.com>
2024-05-15 00:47:42 +0800

    update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -4,11 +4,11 @@ from apex.contrib.layer_norm.layer_norm import FastRMSNormFN, FastLayerNormFN
 from faster_norm import faster_rms_norm, faster_layer_norm
 
 
-# torch.manual_seed(42)
+torch.manual_seed(42)
 
-s, b, h = 8192, 1, 8192
+s, b, h = 8192, 1, 12288
 
-hidden_states = (100. + torch.randn(s, b, h, dtype=torch.bfloat16, device="cuda")).requires_grad_()
+hidden_states = (4. + torch.randn(s, b, h, dtype=torch.bfloat16, device="cuda")).requires_grad_()
 weight = (1 + torch.rand(h, dtype=torch.bfloat16, device="cuda")).requires_grad_()
 bias = (1 + torch.rand(h, dtype=torch.bfloat16, device="cuda")).requires_grad_()
 eps = 1e-5
@@ -40,59 +40,71 @@ def fast_layer_norm(x, gamma, beta, epsilon):
     return FastLayerNormFN.apply(x, gamma, beta, epsilon)
 
 
-dtype_ref = torch.float64
-hidden_states_float = hidden_states.detach().to(dtype_ref).requires_grad_()
-weight_float = weight.detach().to(dtype_ref).requires_grad_()
-bias_float = bias.detach().to(dtype_ref).requires_grad_()
-# output_ref = rms_norm(hidden_states_float, weight_float, eps).to(hidden_states.dtype)
-output_ref = layer_norm_compiled(hidden_states_float, weight_float, bias_float, eps).to(hidden_states.dtype)
-output_ref.backward(grad_output)
-dgrad_ref = hidden_states_float.grad.to(hidden_states.dtype)
-wgrad_ref = weight_float.grad.to(weight.dtype)
-bgrad_ref = bias_float.grad.to(bias.dtype)
+for is_layer_norm in [False, True]:
+    dtype_ref = torch.float64
+    hidden_states_float = hidden_states.detach().to(dtype_ref).requires_grad_()
+    weight_float = weight.detach().to(dtype_ref).requires_grad_()
+    bias_float = bias.detach().to(dtype_ref).requires_grad_()
+    if is_layer_norm:
+        output_ref = layer_norm(hidden_states_float, weight_float, bias_float, eps).to(hidden_states.dtype)
+    else:
+        output_ref = rms_norm(hidden_states_float, weight_float, eps).to(hidden_states.dtype)
+    output_ref.backward(grad_output)
+    dgrad_ref = hidden_states_float.grad.to(hidden_states.dtype)
+    wgrad_ref = weight_float.grad.to(weight.dtype)
+    if is_layer_norm:
+        bgrad_ref = bias_float.grad.to(bias.dtype)
 
-
-for do_backward in ["", "[input]", "[input]+[weight]+[bias]"]:
-    weight.requires_grad_("[weight]" in do_backward)
-    bias.requires_grad_("[bias]" in do_backward)
-    for _ in range(5):
-        for name, fn in [
-            # ("rms_norm_native", rms_norm),
-            # ("rms_norm_compiled", rms_norm_compiled),
-            # ("FastRMSNormFN", fast_rms_norm),
-            # ("faster_rms_norm", faster_rms_norm),
-            ("layer_norm_native", layer_norm),
-            ("layer_norm_compiled", layer_norm_compiled),
-            ("FastLayerNormFN", fast_layer_norm),
-            ("faster_layer_norm", faster_layer_norm),
-        ]:
-            warmup_times = 5
-            run_times = 20
-            ev1 = torch.cuda.Event(enable_timing=True)
-            ev2 = torch.cuda.Event(enable_timing=True)
-            for _ in range(warmup_times):
-                hidden_states.grad = None
-                weight.grad = None
-                bias.grad = None
-                output = fn(hidden_states, weight, bias, eps)
+    for do_backward in ["", "[input]", "[input]+[weight]+[bias]"]:
+        weight.requires_grad_("[weight]" in do_backward)
+        bias.requires_grad_("[bias]" in do_backward)
+        for _ in range(3):
+            for name, fn in ([
+                ("layer_norm_native", layer_norm),
+                ("layer_norm_compiled", layer_norm_compiled),
+                ("FastLayerNormFN", fast_layer_norm),
+                ("faster_layer_norm", faster_layer_norm),
+            ] if is_layer_norm else [
+                ("rms_norm_native", rms_norm),
+                ("rms_norm_compiled", rms_norm_compiled),
+                ("FastRMSNormFN", fast_rms_norm),
+                ("faster_rms_norm", faster_rms_norm),
+            ]):
+                warmup_times = 5
+                run_times = 20
+                ev1 = torch.cuda.Event(enable_timing=True)
+                ev2 = torch.cuda.Event(enable_timing=True)
+                for _ in range(warmup_times):
+                    hidden_states.grad = None
+                    weight.grad = None
+                    bias.grad = None
+                    if is_layer_norm:
+                        output = fn(hidden_states, weight, bias, eps)
+                    else:
+                        output = fn(hidden_states, weight, eps)
+                    if do_backward:
+                        output.backward(grad_output)
+                ev1.record()
+                for _ in range(run_times):
+                    hidden_states.grad = None
+                    weight.grad = None
+                    bias.grad = None
+                    if is_layer_norm:
+                        output = fn(hidden_states, weight, bias, eps)
+                    else:
+                        output = fn(hidden_states, weight, eps)
+                    if do_backward:
+                        output.backward(grad_output)
+                ev2.record()
+                ev2.synchronize()
+                dt = ev1.elapsed_time(ev2) / 1000. / run_times
+                bw = hidden_states.numel() * hidden_states.element_size() * (5 if do_backward else 2) / dt
+                print(f"{name:20s}", f"{bw / 2**30:8.3f} GiB/s", end=" ")
+                print(f"{(output - output_ref).abs().mean().item():.3E}", end=" ")
                 if do_backward:
-                    output.backward(grad_output)
-            ev1.record()
-            for _ in range(run_times):
-                hidden_states.grad = None
-                weight.grad = None
-                bias.grad = None
-                output = fn(hidden_states, weight, bias, eps)
-                if do_backward:
-                    output.backward(grad_output)
-            ev2.record()
-            ev2.synchronize()
-            dt = ev1.elapsed_time(ev2) / 1000. / run_times
-            bw = hidden_states.numel() * hidden_states.element_size() * (5 if do_backward else 2) / dt
-            print(f"{name:20s}", f"{bw / 2**30:8.3f} GiB/s", end=" ")
-            print((output - output_ref).abs().mean().item(), end=" ")
-            if do_backward:
-                print((hidden_states.grad - dgrad_ref).abs().mean().item(), end=" ")
-                if weight.requires_grad:
-                    print((weight.grad - wgrad_ref).abs().mean().item(), end=" ")
-            print()
+                    print(f"{(hidden_states.grad - dgrad_ref).abs().mean().item():.3E}", end=" ")
+                    if weight.requires_grad:
+                        print(f"{(weight.grad - wgrad_ref).abs().mean().item():.3E}", end=" ")
+                    if is_layer_norm and bias.requires_grad:
+                        print(f"{(bias.grad - bgrad_ref).abs().mean().item():.3E}", end=" ")
+                print()
